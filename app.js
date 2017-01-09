82c65f4a73a0eda25f6d06464a2e5b33d40ba9df
dhananjay431 <dbel431@gmail.com>
2017-01-09 09:16:29 +0530

    change api

--- app.js
+++ app.js
@@ -1,50 +1,52 @@
 var express = require('express')
+var router = express.Router()
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var path= require('path');
 var app = express();
 
+
+router.use(function (req, res, next) {
+  console.log('Time:', Date.now())
+  console.log('req.baseUrl', req.baseUrl)
+  console.log('req.hostname',req.hostname)
+  console.log('res.get(mytoken)',res.get('mytoken'))
+  
+  res.set({
+  'mytoken': '12345'
+});
+  
+  
+  next()
+})
+app.use('/api/v1/', router)
+
+
+
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use(bodyParser.json());
-app.use('/', express.static(path.join(__dirname, 'public')))
+
+app.use('/', express.static(path.join(__dirname, '/public')))
+app.use('/', express.static(path.join(__dirname, '/bower_components')))
 app.use(bodyParser.json({ type: 'application/*+json' }));
+
 mongoose.connect('mongodb://localhost/test');
-
 var appFormSchema =new mongoose.Schema({}, {strict: false});
-
 var Cat = mongoose.model('Cats',appFormSchema);
 
-
-app.get('/', function (req, res) {
-
-});
-app.get('/db/:dname', function (req, res) {
-Cat.find({name:req.params.dname }).exec(function (err, small) {
+router.get('/db', function (req, res) {
+Cat.find({ name:'dhananjay' }).exec(function (err, small) {
   if (err) 
   return  res.send('error');
   else
   return  res.send(small);
 });
 });
-app.get('/db/:dname', function (req, res) {
-Cat.find({ name:req.params.dname }).exec(function (err, small) {
-  if (err) 
-  return  res.send('error');
-  else
-  return  res.send(small);
-});
-});
-app.post('/db/:dname', function (req, res) {
-console.log("req.body",req.body);
-console.log("req.query",req.query);
-
-console.log("req.params.dname",req.params.dname);
-var query=req.body;
-query.name=req.params.dname;
-console.log(query);
-var CAT=new Cat(query);
+router.post('/db', function (req, res) {
+  
+var CAT=new Cat(req.body);
 CAT.save(function (err) {
   if (err) return res.send(err);
   else
